0dbbbf22f3c0199753c8a1b3116b8323b876c41d
Dkflora <dkflora13@gmail.com>
2026-06-14 16:53:23 -0700

    Update S3 bucket name in backend configuration

--- envs/prod/backend.tf
+++ envs/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-wewe15-dk"
+    bucket       = "you-bucket-name"
     key          = "envs/prod/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true 
